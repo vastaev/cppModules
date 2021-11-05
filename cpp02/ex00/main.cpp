e8735febac0045634a6aa4fba3601ac1440f8ccb
nephilister <olegvastaev@gmail.com>
2021-11-05 23:20:50 +0500

    done

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -8,5 +8,7 @@ c = b;
 std::cout << a.getRawBits() << std::endl;
 std::cout << b.getRawBits() << std::endl;
 std::cout << c.getRawBits() << std::endl;
+c.setRawBits(42);
+std::cout << c.getRawBits() << std::endl;
 return 0;
 }
