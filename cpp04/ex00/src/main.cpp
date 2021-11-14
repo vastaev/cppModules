0dfc63e7f80e5ddebb3b68a1d382c0d0c369611b
cjoanne - Oleg Vastaev <cjoanne@student.21-school.ru>
2021-11-14 13:51:41 +0300

    made dirs

--- cpp04/ex00/src/main.cpp
+++ cpp04/ex00/src/main.cpp
@@ -16,6 +16,7 @@ int main()
 	std::cout << "--GetType" << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	std::cout << j->getType() << " " << std::endl;
+	std::cout << meta->getType() << " " << std::endl;
 	std::cout << "---------------" << std::endl;
 	std::cout << "--MakeSound" << std::endl;
 	i->makeSound(); //will output the cat sound!
