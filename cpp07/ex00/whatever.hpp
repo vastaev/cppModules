22038df1d3cdf1039692e48675d19d5b1158f7d7
cjoanne - Oleg Vastaev <cjoanne@student.21-school.ru>
2021-12-04 13:25:43 +0300

    done 00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,24 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template<typename Type>
+void swap(Type &a, Type &b)
+{
+	Type tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename Type>
+Type max(Type a, Type b)
+{
+	return (a > b ? a : b);
+}
+
+template<typename Type>
+Type min(Type a, Type b)
+{
+	return (a < b ? a : b);
+}
+
+#endif
